d4ea0c21631dc41ae5e043a6bbe7b1221fbcff40
Emantesta <emantesta@gmail.com>
2025-03-11 14:43:48 +0100

    server.js

--- Backend/server.js
+++ Backend/server.js
@@ -20,7 +20,7 @@ const logger = winston.createLogger({
   level: 'info',
   format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
   transports: [
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
     new winston.transports.Console()
   ]
 });
